c5d4e08e2ec6c8146e1c8e3b7115393d159a9cb7
Paul Mansfield <paul-github@mansfield.co.uk>
2017-02-03 17:02:05 +0000

    the configuration values required by the script

---
+++ config.sh
@@ -0,0 +1,11 @@
+# config.sh
+# configuration file for wisepay
+
+USER="Wisepay.Username
+PASS="wisepaypass"
+MID=1234
+ROOTDIR=$HOME/.wisepay
+COOKIEFILE=$ROOTDIR/cookiefile
+HOMEPAGEFILE=$ROOTDIR/homepage.html
+
+
